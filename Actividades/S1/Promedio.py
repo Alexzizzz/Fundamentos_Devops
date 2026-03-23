f8a26091a223830c9d59fb4777f35eb57c58e389
Alexzizzz <alexbzyx.79@icloud.com>
2026-03-22 23:03:58 -0600

    Update Promedio.py

--- Actividades/S1/Promedio.py
+++ Actividades/S1/Promedio.py
@@ -12,16 +12,16 @@ python = int(input(nombre + " ¿Cuál es tu calificación de Python?: "))
 
 promedio = (algoritmos + poo + bases_datos + desarrollo_web + redes + arquitectura + python) / 7
 
-if promedio >= 60:
-    print(" :( " + nombre + " aprobaste por pura suerte: ", round(promedio, 2))
-elif promedio >= 70:
-    print("Que te digo " + nombre + "pero Hechale ganas: ", round(promedio, 2))
-elif promedio >= 80:
-    print("Vas mejorando " + nombre + "No me convences: ", round(promedio, 2))
+if promedio == 100:
+    print("Ya Ahora si te mereces tu mercho " + nombre + " Llegaste al top: ", round(promedio, 2))
 elif promedio >= 90:
-    print("Eso ya esta mejor " + nombre + "pero puedes mejorar: ", round(promedio, 2))
-elif promedio == 100:
-    print("Ya Ahora si te mereces tu mercho" + nombre + "Llegaste al top: ", round(promedio, 2))
+    print("Eso ya esta mejor " + nombre + " pero puedes mejorar: ", round(promedio, 2))
+elif promedio >= 80:
+    print("Vas mejorando " + nombre + " No me convences: ", round(promedio, 2))
+elif promedio >= 70:
+    print("Que te digo " + nombre + " pero Hechale ganas: ", round(promedio, 2))
+elif promedio >= 60:
+    print(":( " + nombre + " aprobaste por pura suerte: ", round(promedio, 2))
 else:
-    print("Que wey " + nombre + " reprobaste JAJAJAJAJAJA: ", round(promedio, 2))
+    print("Que wey " + nombre + " reprobaste JAJAJAJAJA: ", round(promedio, 2))
 print("Fin")
